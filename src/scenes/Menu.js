8220c41deaca89af3ed26b33a1f2ed412658a87a
Ben Quang <100402290+benqUC@users.noreply.github.com>
2022-04-20 02:10:18 -0700

    redesigning game's theme's/aesthetic

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -9,6 +9,7 @@ class Menu extends Phaser.Scene {
         this.load.audio('sfx_explosion', './assets/explosion38.wav');
         this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
         this.load.audio('bgm_play', './assets/bensound-newdawn.mp3')
+        this.load.image('starfield', './assets/sky.png');
     }
 
     create() {
@@ -25,9 +26,12 @@ class Menu extends Phaser.Scene {
             },
             fixedWidth: 0
         }
+
+        // place tile sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
         
         // show menu text
-        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'AIR PATROL', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
         menuConfig.backgroundColor = '#00FF00';
         menuConfig.color = '#000';
